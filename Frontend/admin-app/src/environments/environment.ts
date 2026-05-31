bf8c493217e8e212ccffda5e0eb5fcd232a3a381
יאיר חליבה <myh21212@gmail.com>
2026-05-31 14:29:22 +0300

    עדכון environment

--- Frontend/admin-app/src/environments/environment.ts
+++ Frontend/admin-app/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     apiBaseUrl: 'https://localhost:44395',
     sheetMusicEnabled: false,
-    imageTransformationsEnabled: false
+    imageTransformationsEnabled: true
 };
